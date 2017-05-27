6a05a69e57be212cbec3d1d1d8a1449e6a0d99c0
Sho Takemori <stakemorii@gmail.com>
2017-05-27 13:22:44 +0200

    Add factor

--- src/bindings.rs
+++ src/bindings.rs
@@ -10,6 +10,15 @@ pub type fmpz_t = [fmpz; 1usize];
 pub type fmpzptr = *mut fmpz;
 pub type fmpzconstptr = *const fmpz;
 
+#[repr(C)]
+pub struct fmpz_factor_struct {
+    pub sign: c_int,
+    pub p: *mut fmpz,
+    pub exp: *mut mp_limb_t,
+    pub alloc: mp_limb_signed_t,
+    pub num: mp_limb_signed_t,
+}
+
 #[link(name = "flint_wrapper")]
 extern "C" {
     pub fn fmpz_add(f: fmpzptr, g: fmpzconstptr, h: fmpzconstptr);
@@ -23,4 +32,7 @@ extern "C" {
     pub fn fmpz_get_str(str: *mut c_char, b: c_int, f: fmpzconstptr) -> *const c_char;
     pub fn fmpz_sizeinbase(f: fmpzconstptr, b: c_int) -> usize;
     pub fn fmpz_pow_ui(f: fmpzptr, g: fmpzconstptr, exp: mp_limb_t);
+    pub fn fmpz_factor_init(factor: *mut fmpz_factor_struct);
+    pub fn fmpz_factor_clear(factor: *mut fmpz_factor_struct);
+    pub fn fmpz_factor(factor: *mut fmpz_factor_struct, n: fmpzconstptr);
 }
