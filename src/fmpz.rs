5efea2c3c2b66f4b7a5249a883e50ca5ac5ed9ff
Sho Takemori <stakemorii@gmail.com>
2017-05-27 11:35:24 +0200

    Use fmpzptr

--- src/fmpz.rs
+++ src/fmpz.rs
@@ -15,11 +15,11 @@ impl Drop for Fmpz {
 }
 
 impl Fmpz {
-    fn as_mut_ptr(&mut self) -> *mut fmpz {
+    fn as_mut_ptr(&mut self) -> fmpzptr {
         self.fmpz.as_mut_ptr()
     }
 
-    fn as_ptr(&self) -> *const fmpz {
+    fn as_ptr(&self) -> fmpzconstptr {
         self.fmpz.as_ptr()
     }
 
