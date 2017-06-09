2181edf74eabf89afe2ffcd2105d69ab6c697df6
Sho Takemori <stakemorii@gmail.com>
2017-06-09 21:39:58 +0200

    Add test for factor

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,10 +1,11 @@
 extern crate flint;
 extern crate num;
+extern crate libc;
 
+use libc::{c_ulong};
 use num::FromPrimitive;
 use num::bigint::BigInt;
-
-use flint::fmpz::{Fmpz};
+use flint::fmpz::{Fmpz, FmpzFactor};
 
 #[test]
 fn add_test() {
@@ -27,17 +28,26 @@ fn add_test() {
     }
 }
 
-// #[test]
-// fn it_works() {
-//     let mut res = Fmpz::new();
-//     let a = Fmpz::from_str("239023902390239032920930920", 10).unwrap();
-//     let b = Fmpz::from_si(344349839938948);
-//     res.set_mul(&a, &b);
-//     println!("res1={}", res);
-//     res.set_mul_ui(&a, 10);
-//     println!("res2={}", res);
-//     res.pow_ui(&a, 12);
-//     println!("{}", res);
-//     println!("{:?}", res.factor());
-// }
+fn fac_to_fmpz(f: &FmpzFactor) -> Fmpz {
+    let mut res = Fmpz::from_si(1);
+    let mut tmp = Fmpz::new();
+    for &(ref a, e) in f.to_vec().iter() {
+        tmp.pow_ui(&a, e as c_ulong);
+        res *= &tmp;
+    }
+    res
+}
 
+#[test]
+fn it_works() {
+    let mut a = Fmpz::from_str("1844674407370955161", 10).unwrap();
+    let b = Fmpz::from_si(340394);
+    a -= &b;
+    // println!("res1={}", res);
+    // res.set_mul_ui(&a, 10);
+    // println!("res2={}", res);
+    // res.pow_ui(&a, 12);
+    // println!("{}", res);
+    let fac = a.to_factor();
+    assert!(fac_to_fmpz(&fac).get_str(10) == a.get_str(10));
+}
