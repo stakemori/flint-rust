55be208fc3cca1935805ebb3e724071e3b639a3c
Sho Takemori <stakemorii@gmail.com>
2017-09-01 14:36:08 +0200

    Add jacobi symbol bench

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,31 +1,22 @@
 #![feature(test)]
+extern crate libc;
 
+use libc::c_ulong;
 extern crate test;
 extern crate flint;
 
 use flint::fmpz::Fmpz;
 use test::Bencher;
-use flint::bindings::bench_square_sum_native;
 
-fn square_sum(n: u64) {
-    let mut res = Fmpz::from_ui(0);
-    let mut a = Fmpz::new();
-    let mut tmp = Fmpz::new();
-    for i in 1..n {
-        a.set_ui(i);
-        tmp.pow_ui_mut(&a, 2);
-        res += &tmp;
-    }
-}
 
 #[bench]
-fn square_sum_bench(b: &mut Bencher) {
-    b.iter(|| square_sum(1000000))
-}
-
-#[bench]
-fn square_sum_native_bench(b: &mut Bencher) {
-    b.iter(|| unsafe {
-        bench_square_sum_native(1000000);
+fn jacobi_bench(b: &mut Bencher) {
+    b.iter(|| {
+        let p: Fmpz = From::from(1031 as c_ulong);
+        let mut a: Fmpz = From::from(1 as c_ulong);
+        while a < p {
+            a.jacobi(&p);
+            a += 1;
+        }
     })
 }
