415c88a499e2e3639637ae19f92ed65a7fe13446
Sho Takemori <stakemorii@gmail.com>
2017-09-01 17:04:15 +0200

    Refactor code

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -2,18 +2,20 @@
 extern crate test;
 extern crate flint;
 
-use flint::fmpz::Fmpz;
 use test::Bencher;
 
-
-#[bench]
-fn jacobi_bench(b: &mut Bencher) {
-    b.iter(|| {
-        let p: Fmpz = From::from(1031);
-        let mut a: Fmpz = From::from(1);
-        while a < p {
-            a.jacobi(&p);
-            a += 1;
-        }
-    })
+mod fmpz {
+    use super::*;
+    use flint::fmpz::Fmpz;
+    #[bench]
+    fn jacobi_bench(b: &mut Bencher) {
+        b.iter(|| {
+            let p: Fmpz = From::from(1031);
+            let mut a: Fmpz = From::from(1);
+            while a < p {
+                a.jacobi(&p);
+                a += 1;
+            }
+        })
+    }
 }
