844fd34dd1a7b12af4c92a6c2f0dbfab9d3a801c
Sho Takemori <stakemorii@gmail.com>
2017-09-01 17:03:30 +0200

    Fix code

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,7 +1,4 @@
 #![feature(test)]
-extern crate libc;
-
-use libc::c_ulong;
 extern crate test;
 extern crate flint;
 
@@ -12,8 +9,8 @@ use test::Bencher;
 #[bench]
 fn jacobi_bench(b: &mut Bencher) {
     b.iter(|| {
-        let p: Fmpz = From::from(1031 as c_ulong);
-        let mut a: Fmpz = From::from(1 as c_ulong);
+        let p: Fmpz = From::from(1031);
+        let mut a: Fmpz = From::from(1);
         while a < p {
             a.jacobi(&p);
             a += 1;
